75cf64b2af1805b43ec63f97d7b39ed21fdfe36d
xt <118093396@qq.com>
2017-12-11 11:32:18 +0800

    no message

--- src/api_request/行为洞察/个人/(行为洞察-个人-获取图表数据)action_personal_echart.js
+++ src/api_request/行为洞察/个人/(行为洞察-个人-获取图表数据)action_personal_echart.js
@@ -23,10 +23,12 @@
   "data" : [
     {
       value: '100',
-      name: '男'
+      name: '男',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '女'
+      name: '女',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -38,31 +40,40 @@
   "data" : [
     {
       value: '100',
-      name: '18以下'
+      name: '18以下',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '18-22岁'
+      name: '18-22岁',
+      "perValue": 30 //占比
     }, {
       value: '310',
-      name: '23-28岁'
+      name: '23-28岁',
+      "perValue": 30 //占比
     }, {
       value: '410',
-      name: '29-34岁'
+      name: '29-34岁',
+      "perValue": 30 //占比
     }, {
       value: '480',
       name: '35-40岁'
+      "perValue": 30 //占比
     }, {
       value: '420',
       name: '41-46岁'
+      "perValue": 30 //占比
     }, {
       value: '360',
       name: '47-52岁'
+      "perValue": 30 //占比
     }, {
       value: '120',
       name: '52-60岁'
+      "perValue": 30 //占比
     }, {
       value: '100',
       name: '60岁以上'
+      "perValue": 30 //占比
     },
   ],
   "success" : true
@@ -75,12 +86,15 @@
     {
       value: '100',
       name: '黄浦区'
+      "perValue": 30 //占比
     }, {
       value: '150',
       name: '静安区'
+      "perValue": 30 //占比
     }, {
       value: '210',
       name: '宝山区'
+      "perValue": 30 //占比
     },
     .......所有的行政区
   ],
@@ -93,13 +107,16 @@
   "data" : [
     {
       value: '100',
-      name: '黄浦区'
+      name: '黄浦区',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '静安区'
+      name: '静安区',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '宝山区'
+      name: '宝山区',
+      "perValue": 30 //占比
     },
     .......所有的行政区
   ],
@@ -112,13 +129,16 @@
   "data" : [
     {
       value: '100',
-      name: '黄浦区'
+      name: '黄浦区',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '静安区'
+      name: '静安区',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '宝山区'
+      name: '宝山区',
+      "perValue": 30 //占比
     },
     .......所有的行政区
   ],
@@ -131,13 +151,16 @@
   "data" : [
     {
       value: '100',
-      name: '高'
+      name: '高',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '中'
+      name: '中',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '低'
+      name: '低',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -149,13 +172,16 @@
   "data" : [
     {
       value: '100',
-      name: '高'
+      name: '高',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '中'
+      name: '中',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '低'
+      name: '低',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -167,13 +193,16 @@
   "data" : [
     {
       value: '100',
-      name: '高'
+      name: '高',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '中'
+      name: '中',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '低'
+      name: '低',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -185,10 +214,12 @@
   "data" : [
     {
       value: '100',
-      name: '有车'
+      name: '有车',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '无车'
+      name: '无车',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -200,31 +231,40 @@
   "data" : [
     {
       value: '100',
-      name: '2-3w'
+      name: '2-3w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '3-4w'
+      name: '3-4w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '4-5w'
+      name: '4-5w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '5-6w'
+      name: '5-6w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '6-7w'
+      name: '6-7w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '7-8w'
+      name: '7-8w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '8-9w'
+      name: '8-9w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '9-10w'
+      name: '9-10w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '10w以上'
+      name: '10w以上',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -236,49 +276,64 @@
   "data" : [
     {
       value: '100',
-      name: '生活'
+      name: '生活',
+      "perValue": 30 //占比
     }, {
       value: '200',
-      name: '购物'
+      name: '购物',
+      "perValue": 30 //占比
     }, {
       value: '300',
-      name: '休闲娱乐'
+      name: '休闲娱乐',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '科技'
+      name: '科技',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '阅读'
+      name: '阅读',
+      "perValue": 30 //占比
     }, {
       value: '230',
-      name: '视频'
+      name: '视频',
+      "perValue": 30 //占比
     }, {
       value: '340',
-      name: '游戏'
+      name: '游戏',
+      "perValue": 30 //占比
     }, {
       value: '400',
-      name: '教育'
+      name: '教育',
+      "perValue": 30 //占比
     }, {
       value: '500',
-      name: '财经'
+      name: '财经',
+      "perValue": 30 //占比
     }, {
       value: '180',
-      name: '区域'
+      name: '区域',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '体育'
+      name: '体育',
+      "perValue": 30 //占比
     }, {
       value: '290',
-      name: '政治'
+      name: '政治',
+      "perValue": 30 //占比
     }, {
       value: '420',
-      name: '音乐'
+      name: '音乐',
+      "perValue": 30 //占比
     }, {
       value: '310',
-      name: '门户'
+      name: '门户',
+      "perValue": 30 //占比
     }, {
       value: '360',
-      name: '其他'
+      name: '其他',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
--- src/api_request/行为洞察/家庭/(行为洞察-家庭-获取图表数据)action_family_echart.js
+++ src/api_request/行为洞察/家庭/(行为洞察-家庭-获取图表数据)action_family_echart.js
@@ -23,22 +23,28 @@
   "data" : [
     {
       value: '100',
-      name: '1口之家'
+      name: '1口之家',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '2口之家'
+      name: '2口之家',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '3口之家'
+      name: '3口之家',
+      "perValue": 30 //占比
     }, {
       value: '200',
-      name: '4口之家'
+      name: '4口之家',
+      "perValue": 30 //占比
     }, {
       value: '120',
-      name: '5口之家'
+      name: '5口之家',
+      "perValue": 30 //占比
     }, {
       value: '70',
-      name: '其他'
+      name: '其他',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -50,13 +56,16 @@
   "data" : [
     {
       value: '100',
-      name: '高'
+      name: '高',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '中'
+      name: '中',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '低'
+      name: '低',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -68,13 +77,16 @@
   "data" : [
     {
       value: '100',
-      name: '高'
+      name: '高',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '中'
+      name: '中',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '低'
+      name: '低',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -86,13 +98,16 @@
   "data" : [
     {
       value: '100',
-      name: '高'
+      name: '高',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '中'
+      name: '中',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '低'
+      name: '低',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -104,10 +119,12 @@
   "data" : [
     {
       value: '100',
-      name: '有车'
+      name: '有车',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '无车'
+      name: '无车',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -119,31 +136,40 @@
   "data" : [
     {
       value: '100',
-      name: '2-3w'
+      name: '2-3w',
+      "perValue": 30 //占比
     }, {
-      value: '100',
+      value: '100',,
+      "perValue": 30 //占比
       name: '3-4w'
     }, {
       value: '100',
-      name: '4-5w'
+      name: '4-5w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '5-6w'
+      name: '5-6w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '6-7w'
+      name: '6-7w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '7-8w'
+      name: '7-8w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '8-9w'
+      name: '8-9w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '9-10w'
+      name: '9-10w',
+      "perValue": 30 //占比
     }, {
       value: '100',
-      name: '10w以上'
+      name: '10w以上',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
@@ -155,49 +181,64 @@
   "data" : [
     {
       value: '100',
-      name: '生活'
+      name: '生活',
+      "perValue": 30 //占比
     }, {
       value: '200',
-      name: '购物'
+      name: '购物',
+      "perValue": 30 //占比
     }, {
       value: '300',
-      name: '休闲娱乐'
+      name: '休闲娱乐',
+      "perValue": 30 //占比
     }, {
       value: '150',
-      name: '科技'
+      name: '科技',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '阅读'
+      name: '阅读',
+      "perValue": 30 //占比
     }, {
       value: '230',
-      name: '视频'
+      name: '视频',
+      "perValue": 30 //占比
     }, {
       value: '340',
-      name: '游戏'
+      name: '游戏',
+      "perValue": 30 //占比
     }, {
       value: '400',
-      name: '教育'
+      name: '教育',
+      "perValue": 30 //占比
     }, {
       value: '500',
-      name: '财经'
+      name: '财经',
+      "perValue": 30 //占比
     }, {
       value: '180',
-      name: '区域'
+      name: '区域',
+      "perValue": 30 //占比
     }, {
       value: '210',
-      name: '体育'
+      name: '体育',
+      "perValue": 30 //占比
     }, {
       value: '290',
-      name: '政治'
+      name: '政治',
+      "perValue": 30 //占比
     }, {
       value: '420',
-      name: '音乐'
+      name: '音乐',
+      "perValue": 30 //占比
     }, {
       value: '310',
-      name: '门户'
+      name: '门户',
+      "perValue": 30 //占比
     }, {
       value: '360',
-      name: '其他'
+      name: '其他',
+      "perValue": 30 //占比
     }
   ],
   "success" : true
